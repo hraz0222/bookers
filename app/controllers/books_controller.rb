a642deac6849b5938efcf5c74ba917d764504e61
hraz0222 <ec2-user@ip-172-31-21-128.ec2.internal>
2021-07-11 00:11:29 +0000

    バリデーションを実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,7 @@
 class BooksController < ApplicationController
  def index
-  @book = Book.all
- end
-
- def new
-   @book = Book.new
+  @books = Book.all
+  @book = Book.new
  end
 
  def show
@@ -12,9 +9,14 @@ class BooksController < ApplicationController
  end
 
  def create
-  book = Book.new(book_params)
-  if book.save
-    redirect_to book_path(book.id), notice: 'Book was successfully created.'
+  @book = Book.new(book_params)
+
+  if @book.save
+    redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+  else
+    @books = Book.all
+    render :index
+
 
   end
  end
@@ -26,9 +28,12 @@ class BooksController < ApplicationController
  end
 
  def update
-  book = Book.find(params[:id])
-  if book.update(book_params)
-   redirect_to book_path(book.id), notice: 'Book was successfully updated.'
+  @book = Book.find(params[:id])
+
+  if @book.update(book_params)
+   redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+  else
+   render :edit
   end
  end
 
